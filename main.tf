44c5d72499102a6cafc1f6a1abcd15531a089bac
Leonardo Souza <leonardosilva.souza@yahoo.com.br>
2024-10-21 23:25:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ module "wf-instance-01" {
   depends_on = [ module.sg_http ]
 
   name                   = "${var.name}-instance-01"
-  ami_name               = "ami-0324a83b82023f0b3"
+  ami_name               = "ami-windows-basic"
   os_instance            = "windows"
   key_name               = "key-webfarm-windows"
 
@@ -130,7 +130,7 @@ module "wf-instance-02" {
   depends_on = [ module.sg_http ]
 
   name                    = "${var.name}-instance-02"
-  ami_name                = "ami-06b21ccaeff8cd686"
+  ami_name                = "ami-linux-basic"
   os_instance             = "linux"
   key_name                = "key-ec2-linux"
   
